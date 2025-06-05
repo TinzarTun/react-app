97224427a0d2d1a495babdb3d852c9f4bae6290d
TinzarTun <tinzartun2002@gmail.com>
2025-06-05 23:27:52 +0630

    test useEffect()

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type User = {
   name: string;
@@ -8,55 +8,82 @@ type User = {
 export default function About() {
   // hooks
   // const app = useState(100);
-  // const [count, setCount] = useState<number>(100);
-  const [user, setUser] = useState<User>({
-    name: "",
-    age: 0,
-    email: "",
-  });
+  const [count, setCount] = useState<number>(100);
+  // const [user, setUser] = useState<User>({
+  //   name: "",
+  //   age: 0,
+  //   email: "",
+  // });
   // console.log('app', app);
   // count for state data
   // setCount for updating state data
   // console.log("count", count);
   // console.log("setCount", setCount);
-  // function increment() {
-  //   // setCount(count + 1);
-  //   // setCount(count + 1);
-  //   // setCount(count + 1);
-  //   // setCount(count + 1);
-  //   // setCount(count + 1);
-  //   setCount((prevCount) => prevCount + 1);
-  //   setCount((prevCount) => prevCount + 1);
-  //   setCount((prevCount) => prevCount + 1);
-  //   setCount((prevCount) => prevCount + 1);
-  //   setCount((prevCount) => prevCount + 1);
+  function increment() {
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+  }
+  function decrement() {
+    // setCount(count - 1);
+    // setCount(count - 1);
+    // setCount(count - 1);
+    // setCount(count - 1);
+    // setCount(count - 1);
+    setCount((prevCount) => prevCount - 1);
+    setCount((prevCount) => prevCount - 1);
+    setCount((prevCount) => prevCount - 1);
+    setCount((prevCount) => prevCount - 1);
+    setCount((prevCount) => prevCount - 1);
+  }
+  // function handleChangeName(e: React.ChangeEvent<HTMLInputElement>) {
+  //   setUser({ ...user, name: e.target.value });
   // }
-  // function decrement() {
-  //   // setCount(count - 1);
-  //   // setCount(count - 1);
-  //   // setCount(count - 1);
-  //   // setCount(count - 1);
-  //   // setCount(count - 1);
-  //   setCount((prevCount) => prevCount - 1);
-  //   setCount((prevCount) => prevCount - 1);
-  //   setCount((prevCount) => prevCount - 1);
-  //   setCount((prevCount) => prevCount - 1);
-  //   setCount((prevCount) => prevCount - 1);
+  // function handleChangeAge(e: React.ChangeEvent<HTMLInputElement>) {
+  //   setUser({ ...user, age: Number(e.target.value) });
   // }
-  function handlerChangeName(e: React.ChangeEvent<HTMLInputElement>) {
-    setUser({ ...user, name: e.target.value });
-  }
-  function handlerChangeAge(e: React.ChangeEvent<HTMLInputElement>) {
-    setUser({ ...user, age: Number(e.target.value) });
-  }
-  function handlerChangeEmail(e: React.ChangeEvent<HTMLInputElement>) {
-    setUser({ ...user, email: e.target.value });
-  }
+  // function handleChangeEmail(e: React.ChangeEvent<HTMLInputElement>) {
+  //   setUser({ ...user, email: e.target.value });
+  // }
+  // function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  //   setUser({
+  //     ...user,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // }
+  // function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
+  //   console.log("User submitted:", user);
+  // }
+
+  //useEffect
+  const [name, setName] = useState<string>("");
+  useEffect(() => {
+    // This code runs when the component mounts
+    console.log("useEffect");
+    // setName("Rosella");
+  }, [count]); // Empty dependency array means this runs once
+
   return (
     <div className="min-h-screen">
       <h1>This is About Page</h1>
-      {/* <p>Count : {count}</p> */}
-      {/* <button
+      <p>Name : {name}</p>
+      <input
+        type="text"
+        className="border-2"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>Count : {count}</p>
+      <button
         className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
         onClick={increment}
       >
@@ -67,8 +94,8 @@ export default function About() {
         onClick={decrement}
       >
         Decrement
-      </button> */}
-      <div>
+      </button>
+      {/* <div>
         <h2 className="text-lg font-semibold">User Information</h2>
         <p>
           <strong>Name:</strong> {user.name}
@@ -79,35 +106,49 @@ export default function About() {
         <p>
           <strong>Email:</strong> {user.email}
         </p>
-      </div>
-      <div>
-        <input
-          className="border border-gray-300 rounded px-2 py-1 mb-2 block"
-          type="text"
-          value={user.name}
-          onChange={handlerChangeName}
-        />
-        <input
-          className="border border-gray-300 rounded px-2 py-1 mb-2 block"
-          type="number"
-          value={user.age}
-          onChange={handlerChangeAge}
-        />
-        <input
-          className="border border-gray-300 rounded px-2 py-1 mb-2 block"
-          type="text"
-          value={user.email}
-          onChange={handlerChangeEmail}
-        />
-        <button
-          className="bg-emerald-500 text-white px-4 py-2 rounded mr-2"
-          onClick={() =>
-            setUser({ name: user.name, age: user.age, email: user.email })
-          }
+      </div> */}
+      {/* <div> */}
+      {/* <form
+          action=""
+          className="p-4 border border-gray-300 rounded"
+          onSubmit={handleSubmit}
         >
-          Update
-        </button>
-      </div>
+          <input
+            className="border border-gray-300 rounded px-2 py-1 mb-2 block"
+            type="text"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+          <input
+            className="border border-gray-300 rounded px-2 py-1 mb-2 block"
+            type="number"
+            name="age"
+            value={user.age}
+            onChange={handleChange}
+          />
+          <input
+            className="border border-gray-300 rounded px-2 py-1 mb-2 block"
+            type="text"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <input
+            type="submit"
+            className="bg-emerald-500 text-white px-4 py-2 rounded mr-2"
+            value="Login"
+          />
+        </form> */}
+      {/* <button
+            className="bg-emerald-500 text-white px-4 py-2 rounded mr-2"
+            onClick={() =>
+              setUser({ name: user.name, age: user.age, email: user.email })
+            }
+          >
+            Update
+          </button> */}
+      {/* </div> */}
     </div>
   );
 }
